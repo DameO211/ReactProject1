c54bf5f990d328550f2ec5b5c2adb6543d733179
Dame <DameO211@gmail.com>
2023-03-07 21:27:47 -0500

    Starting project.

---
+++ src/Header.js
@@ -0,0 +1,37 @@
+import './Header.css';
+import SearchIcon from '@mui/icons-material/Search';
+
+function Header(){
+    return (
+        <div className='Header'>
+
+            <div className='header__left'>
+                gppdf
+
+
+            </div>
+
+
+
+
+            <div className='header__right'>
+                hellop
+
+            </div>
+
+
+
+
+
+            <div className='header_search'>
+                <SearchIcon/>
+
+            </div>
+
+
+
+
+        </div>
+    );
+}
+export default Header;
