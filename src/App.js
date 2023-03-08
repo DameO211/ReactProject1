f635986b64dcd07060b1782ab6ac318a10e114bf
Dame <dameo211@gmail.com>
2023-03-08 19:47:09 +0000

    Update 5 files. work from work computer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import './App.css';
-import Header from './Header'
+import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-
-
+      <Header />
     </div>
-
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,37 +1,32 @@
-import './Header.css';
-import SearchIcon from '@mui/icons-material/Search';
+import "./Header.css";
+import HeaderOption from "./HeaderOption";
+import SearchIcon from "@mui/icons-material/Search";
+import HomeIcon from "@mui/icons-material/Home";
+import ApartmentIcon from "@mui/icons-material/Apartment";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import RestaurantIcon from "@mui/icons-material/Restaurant";
+import StorefrontIcon from "@mui/icons-material/Storefront";
 
-function Header(){
-    return (
-        <div className='Header'>
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img src="https://picsvg.com/svg/gcejGn01.svg?v=691" alt=" " />
 
-            <div className='header__left'>
-                gppdf
-
-
-            </div>
-
-
-
-
-            <div className='header__right'>
-                hellop
-
-            </div>
-
-
-
-
-
-            <div className='header_search'>
-                <SearchIcon/>
-
-            </div>
-
-
-
-
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" />
         </div>
-    );
+      </div>
+
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={ApartmentIcon} title="Renting" />
+        <HeaderOption Icon={AttachMoneyIcon} title="Home Buying" />
+        <HeaderOption Icon={RestaurantIcon} title="Restaurant" />
+        <HeaderOption Icon={StorefrontIcon} title="Store" />
+      </div>
+    </div>
+  );
 }
 export default Header;
